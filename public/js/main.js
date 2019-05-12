03f1cbda28bffd17c341b0bd3288af6f0ee7c1de
herokunt <herokunt@gmail.com>
2019-05-13 05:54:53 +1000

    test

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ document.getElementById('weather-form').addEventListener('submit', (e) => {
   temp.textContent = ''
   rain.textContent = ''
 
-  fetch(`/weather?location=${location}`)
+  fetch('/weather?location= ' + location)
     .then(res => res.json())
     .then(data => {
       if(data.error){
